946e473d1a5a247149337e72e6692ce231037b68
guruprasad403 <guruprasas27@gmail.com>
2024-12-11 17:43:43 +0530

    Herosection issue

--- frontend/Protfolio/src/Components/Hero.jsx
+++ frontend/Protfolio/src/Components/Hero.jsx
@@ -24,7 +24,7 @@ function HeroSection() {
             },
           });
 
-          if (response.json) {
+          if (response.status) {
             const data = await response.json();
             setUserData(data.users[0]); // Assuming we're only fetching one user
             setLoading(false); // Set loading to false after data is fetched
